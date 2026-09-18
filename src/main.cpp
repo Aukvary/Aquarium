e1c6a121c233f73478ed7ac76d224097d5a12c1e
aukvary <al3xpop123@gmail.com>
2026-09-18 13:12:49 +0700

    remove userver template

--- src/main.cpp
+++ src/main.cpp
@@ -2,13 +2,9 @@
 #include <userver/components/minimal_server_component_list.hpp>
 #include <userver/congestion_control/component.hpp>
 #include <userver/testsuite/testsuite_support.hpp>
+#include <userver/ugrpc/server/component_list.hpp>
 #include <userver/utest/using_namespace_userver.hpp>
 #include <userver/utils/daemon_run.hpp>
-
-#include <userver/ugrpc/server/component_list.hpp>
-
-#include <greeter_service.hpp>
-#include <hello_handler.hpp>
 
 int main(int argc, char* argv[]) {
     const auto component_list =
@@ -16,8 +12,6 @@ int main(int argc, char* argv[]) {
             .Append<components::TestsuiteSupport>()
             .Append<clients::dns::Component>()
             .Append<congestion_control::Component>()
-            .AppendComponentList(ugrpc::server::MinimalComponentList())
-            .Append<aquarium::GreeterServiceComponent>()
-            .Append<aquarium::HelloHandler>();
+            .AppendComponentList(ugrpc::server::MinimalComponentList());
     return utils::DaemonMain(argc, argv, component_list);
 }
